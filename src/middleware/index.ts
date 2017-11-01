a695db23aed8841eacd8fd9f2dd55ab6e1b460ef
timsuchanek <tim.suchanek@gmail.com>
2017-11-01 17:35:24 +0100

    build cleanup

--- src/middleware/index.ts
+++
@@ -1,6 +0,0 @@
-import { default as express } from './express'
-import { default as hapi } from './hapi'
-import { default as koa } from './koa'
-import { default as renderPlaygroundPage } from './render-playground-page'
-
-export { express, hapi, koa, renderPlaygroundPage }
